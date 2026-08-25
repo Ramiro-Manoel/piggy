4716c9bebd86cafce13ab2013487d3e2b7dbbf63
Ramiro Manoel <ramiro.manoel77@gmail.com>
2026-08-24 23:44:43 -0300

    update InMemoryTransactionRepository to TransactionRepository

--- internal/adapters/storage/memory/transaction.go
+++ internal/adapters/storage/memory/transaction.go
@@ -5,22 +5,22 @@ import (
 	"github.com/Ramiro-Manoel/piggy/internal/transaction"
 )
 
-var _ transaction.Repository = (*InMemoryTransactionRepository)(nil)
+var _ transaction.Repository = (*transactionRepository)(nil)
 
-type InMemoryTransactionRepository struct {
+type transactionRepository struct {
 	transactions []transaction.Transaction
 }
 
-func NewInMemoryTransactionRepository() *InMemoryTransactionRepository {
-	return &InMemoryTransactionRepository{transactions: make([]transaction.Transaction, 0)}
+func NewtransactionRepository() *transactionRepository {
+	return &transactionRepository{transactions: make([]transaction.Transaction, 0)}
 }
 
-func (r *InMemoryTransactionRepository) Save(t transaction.Transaction) error {
+func (r *transactionRepository) Save(t transaction.Transaction) error {
 	r.transactions = append(r.transactions, t)
 	return nil
 }
 
-func (r *InMemoryTransactionRepository) Read(id string) (transaction.Transaction, error) {
+func (r *transactionRepository) Read(id string) (transaction.Transaction, error) {
 	for i := range r.transactions {
 		if r.transactions[i].ID == id {
 			return r.transactions[i], nil
@@ -29,7 +29,7 @@ func (r *InMemoryTransactionRepository) Read(id string) (transaction.Transaction
 	return transaction.Transaction{}, fmt.Errorf("transaction with id %s not found", id)
 }
 
-func (r *InMemoryTransactionRepository) List() []transaction.Transaction {
+func (r *transactionRepository) List() []transaction.Transaction {
 	transactions := append([]transaction.Transaction{}, r.transactions...)
 	return transactions
 }
