4b8e0338238170a8fecc807283e820c3bff187f6
Ramiro Manoel <ramiro.manoel77@gmail.com>
2026-08-24 23:53:29 -0300

    adjust in transaction memory repo rename

--- internal/adapters/storage/memory/transaction.go
+++ internal/adapters/storage/memory/transaction.go
@@ -5,22 +5,22 @@ import (
 	"github.com/Ramiro-Manoel/piggy/internal/transaction"
 )
 
-var _ transaction.Repository = (*transactionRepository)(nil)
+var _ transaction.Repository = (*TransactionRepository)(nil)
 
-type transactionRepository struct {
+type TransactionRepository struct {
 	transactions []transaction.Transaction
 }
 
-func NewtransactionRepository() *transactionRepository {
-	return &transactionRepository{transactions: make([]transaction.Transaction, 0)}
+func NewTransactionRepository() *TransactionRepository {
+	return &TransactionRepository{transactions: make([]transaction.Transaction, 0)}
 }
 
-func (r *transactionRepository) Save(t transaction.Transaction) error {
+func (r *TransactionRepository) Save(t transaction.Transaction) error {
 	r.transactions = append(r.transactions, t)
 	return nil
 }
 
-func (r *transactionRepository) Read(id string) (transaction.Transaction, error) {
+func (r *TransactionRepository) Read(id string) (transaction.Transaction, error) {
 	for i := range r.transactions {
 		if r.transactions[i].ID == id {
 			return r.transactions[i], nil
@@ -29,7 +29,7 @@ func (r *transactionRepository) Read(id string) (transaction.Transaction, error)
 	return transaction.Transaction{}, fmt.Errorf("transaction with id %s not found", id)
 }
 
-func (r *transactionRepository) List() []transaction.Transaction {
+func (r *TransactionRepository) List() []transaction.Transaction {
 	transactions := append([]transaction.Transaction{}, r.transactions...)
 	return transactions
 }
