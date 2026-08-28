e479d788a08dbd4bb7fc3b645ff1200810cfd3ef
Ramiro Manoel <ramiro.manoel77@gmail.com>
2026-08-28 00:50:19 -0300

    add postgres in main - add .env file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/Ramiro-Manoel/piggy/internal/account"
 	"github.com/Ramiro-Manoel/piggy/internal/adapters/storage/memory"
+	"github.com/Ramiro-Manoel/piggy/internal/adapters/storage/postgres"
 	"github.com/Ramiro-Manoel/piggy/internal/category"
 	"github.com/Ramiro-Manoel/piggy/internal/handler"
 	"github.com/Ramiro-Manoel/piggy/internal/transaction"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close(context.Background())
 
 	transactionRepo := memory.NewTransactionRepository()
 	categoryRepo := memory.NewCategoryRepository()
-	accountRepo := memory.NewAccountRepository()
+	accountRepo := postgres.NewAccountRepository(conn)
 
 	transactionSvc := transaction.NewService(transactionRepo)
 	categorySvc := category.NewService(categoryRepo)
