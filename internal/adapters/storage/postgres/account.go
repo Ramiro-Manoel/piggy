d1f20e7546aa59b2979ced9f86fd273d84c10066
Ramiro Manoel <ramiro.manoel77@gmail.com>
2026-08-30 00:07:10 -0300

    add scan private method to postgres.account

--- internal/adapters/storage/postgres/account.go
+++ internal/adapters/storage/postgres/account.go
@@ -15,6 +15,15 @@ func NewAccountRepository(db *pgx.Conn) *accountRepository {
 	return &accountRepository{db: db}
 }
 
+func (r *accountRepository) scan(row pgx.Row) (account.Account, error) {
+	var a account.Account
+	err := row.Scan(&a.ID, &a.Name, &a.Number, &a.Owner, &a.Balance)
+	if err != nil {
+		return account.Account{}, err
+	}
+	return a, nil
+
+}
 func (r *accountRepository) Save(a account.Account) error {
 	_, err := r.db.Exec(context.Background(), `
 		INSERT INTO accounts (id, name, number, owner, balance)
@@ -25,14 +34,12 @@ func (r *accountRepository) Save(a account.Account) error {
 }
 
 func (r *accountRepository) Read(id string) (account.Account, error) {
-	var a account.Account
-
 	row := r.db.QueryRow(context.Background(), `
 	SELECT * FROM accounts 
 		WHERE id = $1
 	`, id)
 
-	err := row.Scan(&a.ID, &a.Name, &a.Number, &a.Owner, &a.Balance)
+	a, err := r.scan(row)
 	if err != nil {
 		return account.Account{}, err
 	}
@@ -50,8 +57,7 @@ func (r *accountRepository) List() []account.Account {
 
 	var accounts []account.Account
 	for rows.Next() {
-		var a account.Account
-		err = rows.Scan(&a.ID, &a.Name, &a.Number, &a.Owner, &a.Balance)
+		a, err := r.scan(rows)
 		if err != nil {
 			return []account.Account{}
 		}
